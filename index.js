110bbc1b2f7f6363bba5e593ab1827d413996f0c
devenkumar1 <devenkumar540@gmail.com>
2025-02-13 16:56:59 +0530

    update cors origin

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import cookieParser from 'cookie-parser';
 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL, 
+  origin: process.env.FRONTEND_URL,
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use(cookieParser());
